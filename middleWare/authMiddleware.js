0935c0afaa0f5291f6d7b94fb756cbc8fd28d2e9
Bhanu2509 <76513185+Bhanu2509@users.noreply.github.com>
2023-02-04 14:14:13 +0530

    Update authMiddleware.js

--- middleWare/authMiddleware.js
+++ middleWare/authMiddleware.js
@@ -2,7 +2,19 @@ const asyncHandler = require("express-async-handler");
 const User = require("../models/userModel");
 const jwt = require("jsonwebtoken");
 
+const protect = asyncHandler(async (req, res, next) => {
 
+  try {
+
+    const token = req.cookies.token;
+
+    if (!token) {
+
+      res.status(401);
+
+      throw new Error("Not authorized, please login");
+
+    }
 
     // Verify Token
     const verified = jwt.verify(token, process.env.JWT_SECRET);
@@ -21,4 +33,4 @@ const jwt = require("jsonwebtoken");
   }
 });
 
-
+module.exports = protect;
