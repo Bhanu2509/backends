feed7afb3f874c72f7052b27b35e8236d3340cb3
Bhanu2509 <76513185+Bhanu2509@users.noreply.github.com>
2023-02-06 13:58:24 +0545

    Update authMiddleware.js

--- middleWare/authMiddleware.js
+++ middleWare/authMiddleware.js
@@ -13,7 +13,7 @@ const protect = asyncHandler(async (req, res, next) => {
     // Verify Token
     const verified = jwt.verify(token, process.env.JWT_SECRET);
     // Get user id from token
-    const user = await User.findById(verified.id).select("password");
+    const user = await User.findById(verified.id).select("-password");
 
     if (!user) {
       res.status(401);
